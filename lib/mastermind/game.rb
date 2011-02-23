950488459220c8bfb5fa3bd78c0bd615e6a46457
Fernando Almeida <fernando@fernandoalmeida.net>
2011-02-23 00:55:17 -0300

    Feature "Code-breaker starts game" completa

---
+++ lib/mastermind/game.rb
@@ -0,0 +1,16 @@
+module Mastermind
+
+	class Game
+
+		def initialize(messenger)
+			@messenger = messenger
+		end
+
+		def start
+			@messenger.puts "Welcome to Mastermind!"
+			@messenger.puts "Enter guess:"
+		end
+		
+	end
+
+end
